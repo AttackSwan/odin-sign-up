2aaea3146fef3dfb1bff7b39d23a4a3a207f3e97
AttackSwan <124640611+AttackSwan@users.noreply.github.com>
2023-04-11 13:52:58 +1000

    Add password mismatch warning

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-
+const confirmLabel = document.querySelector('.confirm-text > label');
 const passwordInput = document.getElementById("password");
 const confirmInput = document.getElementById("confirm");
 
-const testButton = document.querySelector('.testButton');
-
-testButton.onclick = () => console.log(confirmPassword());
-
 function confirmPassword(){
     let validation = false;
-
+    //only check when something is in the confirm password input
     if (confirmInput.value && passwordInput.value === confirmInput.value){
         validation = true;
+        confirmText(validation);
     }
-    
+    else if (confirmInput.value && passwordInput.value !== confirmInput.value){
+        confirmText(validation);
+    }
     return validation;
+}
+
+function confirmText(validity){     //Change label text if password mismatch
+    if (validity){
+        confirmLabel.textContent = "PASSWORD CONFIRMED"
+        confirmLabel.style.color = 'green';
+    }
+    else if (!validity){
+        confirmLabel.textContent = "PASSWORDS DO NOT MATCH!"
+        confirmLabel.style.color = 'red';
+    }
+    else{
+        console.log("confirmText() argument error");
+    }
 }
